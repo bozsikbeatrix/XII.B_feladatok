ce5fbe85d65928f1947bcbeb740cda37943e905c
bozsikbeatrix <91243617+bozsikbeatrix@users.noreply.github.com>
2023-03-03 07:35:26 +0200

    nxn matrix mellekatlo n jobbra balra csokk

---
+++ lab21.03.03/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int v[100][100];
+    int n;
+    cout << "n=";
+    cin >> n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i+j<=n-1){
+                v[i][j]=(i+j+1);
+            }else{
+                v[i][j]=(n-1)-(i+j)+(n);
+            }
+            cout << v[i][j] << " ";
+        }
+       cout << endl;
+    }
+    return 0;
+}
